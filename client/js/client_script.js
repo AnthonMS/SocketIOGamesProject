9150d41dfe75137c6c49de48b3a9ea8acf6f9b0e
AnthonMS <Anthon@Steiness.info>
2017-11-13 23:07:26 +0100

    Added chat html and styling.

--- client/js/client_script.js
+++ client/js/client_script.js
@@ -2,4 +2,6 @@ var socket_G = io.connect('http://localhost:4000');
 $(document).ready(function () {
     const client_connect = new Client_connect();
     //console.log(socket_G.id);
+    const $message = ('<p class="message"><b>- Anthon:</b> What are you doing?</p>');
+    $('#message_container').append($message);
 });
