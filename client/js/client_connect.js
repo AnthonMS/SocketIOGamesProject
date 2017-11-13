745e73cf789077c3881485aadd368d27511b92f1
AnthonMS <Anthon@Steiness.info>
2017-11-13 15:38:04 +0100

    Added connection handler and index.html
    Connection handler is going to handle all the socket calls.

--- client/js/client_connect.js
+++ client/js/client_connect.js
@@ -1,5 +1,15 @@
-var socket = io.connect('http://localhost:4000');
+//
 
+/*
 $(document).ready(function () {
     console.log(socket.id);
-});
+});*/
+
+class Client_connect
+{
+    constructor()
+    {
+        //socket_G = io.connect('http://localhost:4000');
+        console.log(socket_G.id);
+    }
+}
---
+++ client/js/client_script.js
@@ -0,0 +1,5 @@
+var socket_G = io.connect('http://localhost:4000');
+$(document).ready(function () {
+    const client_connect = new Client_connect();
+    //console.log(socket_G.id);
+});
